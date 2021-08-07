89617424a4f14450de99958b4a65b53020cc5af1
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-07 16:39:07 +0200

    Add file selector

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Console } from "../Cargo.toml";
 
+
+const emu = new Console(600, 600);
+
+const romSelector = document.getElementById("rom-selector") as HTMLInputElement;
+romSelector.addEventListener("change", function () {
+    this.files.item(0).arrayBuffer().then(rom => {
+        console.log(new Uint8Array(rom));
+    });
+}, false);
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
-const emu = new Console(600, 600);
 
 let old = 0.0;
 
