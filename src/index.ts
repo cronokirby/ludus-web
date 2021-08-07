f6bec525b6da2986a81a04ac22c26a66e5b90cc0
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-07 15:42:26 +0200

    Add time varying canvas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import { draw } from "../Cargo.toml";
+import { Console } from "../Cargo.toml";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
+const emu = new Console(600, 600);
 
-draw(ctx, 600, 600);
+let old = 0.0;
+
+function loop(timestamp: number) {
+    const diff = 1000 * (timestamp - old)
+    old = timestamp;
+    emu.draw(ctx, diff)
+
+    window.requestAnimationFrame(loop);
+}
+
+window.requestAnimationFrame(loop);
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,43 @@ use wasm_bindgen::Clamped;
 use web_sys::{CanvasRenderingContext2d, ImageData};
 
 #[wasm_bindgen]
-pub fn draw(ctx: &CanvasRenderingContext2d, width: u32, height: u32) -> Result<(), JsValue> {
-    let mut data: Vec<u8> = Vec::with_capacity((width * height * 4) as usize);
-    for y in 0..height {
-        for x in 0..width {
-            data.push(0 as u8);
-            data.push(x as u8);
-            data.push(y as u8);
-            data.push(0xFF);
+struct Console {
+    elapsed_micros: u32,
+    width: usize,
+    height: usize,
+    pixels: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl Console {
+    #[wasm_bindgen(constructor)]
+    pub fn new(width: u32, height: u32) -> Self {
+        Console {
+            elapsed_micros: 0,
+            width: width as usize,
+            height: height as usize,
+            pixels: vec![0; (width * height * 4) as usize],
         }
     }
-    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut data), width, height)?;
-    ctx.put_image_data(&data, 0.0, 0.0)
+
+    #[wasm_bindgen]
+    pub fn draw(&mut self, ctx: &CanvasRenderingContext2d, micros: u32) -> Result<(), JsValue> {
+        self.elapsed_micros += micros;
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let start = 4 * (y * self.width + x);
+                self.pixels[start] = (100 * self.elapsed_micros / 1_000_000) as u8;
+                self.pixels[start + 1] = x as u8;
+                self.pixels[start + 2] = y as u8;
+                self.pixels[start + 3] = 0xFF;
+            }
+        }
+        let data = ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&mut self.pixels),
+            self.width as u32,
+            self.height as u32,
+        )?;
+        ctx.put_image_data(&data, 0.0, 0.0)
+    }
 }
