0476c5bad77ecc1b76c46b2eb0aca8d4cc75906c
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-07 18:42:53 +0200

    Add controls

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,64 @@
-import { Console } from "../Cargo.toml";
+import { Emulator } from "../Cargo.toml";
 
-
-const emu = new Console(600, 600);
+const emu = new Emulator();
 
 const romSelector = document.getElementById("rom-selector") as HTMLInputElement;
-romSelector.addEventListener("change", function () {
-    this.files.item(0).arrayBuffer().then(rom => {
-        console.log(new Uint8Array(rom));
-    });
-}, false);
+romSelector.addEventListener(
+  "change",
+  function () {
+    this.files
+      .item(0)
+      .arrayBuffer()
+      .then((rom) => {
+        emu.swap_cart(new Uint8Array(rom));
+      });
+  },
+  false
+);
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
+enum Buttons {
+  A = 1,
+  B = 1 << 1,
+  Start = 1 << 2,
+  Select = 1 << 3,
+  Up = 1 << 4,
+  Down = 1 << 5,
+  Left = 1 << 6,
+  Right = 1 << 7,
+}
+
+let buttons = 0;
+const buttonmap = {
+  j: Buttons.A,
+  k: Buttons.B,
+  h: Buttons.Start,
+  g: Buttons.Select,
+  w: Buttons.Up,
+  s: Buttons.Down,
+  a: Buttons.Left,
+  d: Buttons.Right,
+};
+
+window.addEventListener("keydown", (ev) => {
+  buttons |= buttonmap[ev.key] ?? 0;
+  emu.update_buttons(buttons);
+});
+window.addEventListener("keyup", (ev) => {
+  buttons &= ~(buttonmap[ev.key] ?? 0);
+  emu.update_buttons(buttons);
+});
+
 let old = 0.0;
 
 function loop(timestamp: number) {
-    const diff = 1000 * (timestamp - old)
-    old = timestamp;
-    emu.draw(ctx, diff)
+  const diff = 1000 * (timestamp - old);
+  old = timestamp;
+  emu.step(ctx, diff);
 
-    window.requestAnimationFrame(loop);
+  window.requestAnimationFrame(loop);
 }
 
 window.requestAnimationFrame(loop);
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,104 @@
+use std::ptr;
+
+use ludus;
+use ludus::NES_HEIGHT;
+use ludus::NES_WIDTH;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use web_sys::{CanvasRenderingContext2d, ImageData};
 
+const SAMPLE_RATE: u32 = 4000;
+
 #[wasm_bindgen]
-struct Console {
-    elapsed_micros: u32,
-    width: usize,
-    height: usize,
-    pixels: Vec<u8>,
+pub fn height() -> u32 {
+    ludus::NES_HEIGHT as u32
 }
 
 #[wasm_bindgen]
-impl Console {
+pub fn width() -> u32 {
+    ludus::NES_WIDTH as u32
+}
+
+struct NullAudioDevice;
+
+impl ludus::AudioDevice for NullAudioDevice {
+    fn push_sample(&mut self, _sample: f32) {}
+}
+
+struct PixelBuffer(Vec<u8>);
+
+impl PixelBuffer {
+    fn new() -> Self {
+        PixelBuffer(vec![0; 4 * NES_HEIGHT * NES_WIDTH])
+    }
+
+    fn render_to(&mut self, ctx: &CanvasRenderingContext2d) -> Result<(), JsValue> {
+        let data = ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&mut self.0),
+            NES_WIDTH as u32,
+            NES_HEIGHT as u32,
+        )?;
+        ctx.put_image_data(&data, 0.0, 0.0)
+    }
+}
+
+impl ludus::VideoDevice for PixelBuffer {
+    fn blit_pixels(&mut self, pixels: &ludus::PixelBuffer) {
+        for (i, pixel) in pixels.as_ref().iter().enumerate() {
+            let base = 4 * i;
+            self.0[base + 3] = (pixel >> 24) as u8;
+            self.0[base] = (pixel >> 16) as u8;
+            self.0[base + 1] = (pixel >> 8) as u8;
+            self.0[base + 2] = *pixel as u8;
+        }
+    }
+}
+
+#[wasm_bindgen]
+struct Emulator {
+    pixels: PixelBuffer,
+    audio: [f32; 3],
+    console: Option<ludus::Console>,
+}
+
+#[wasm_bindgen]
+impl Emulator {
     #[wasm_bindgen(constructor)]
-    pub fn new(width: u32, height: u32) -> Self {
-        Console {
-            elapsed_micros: 0,
-            width: width as usize,
-            height: height as usize,
-            pixels: vec![0; (width * height * 4) as usize],
+    pub fn new() -> Self {
+        Emulator {
+            pixels: PixelBuffer::new(),
+            audio: [0.0, 0.1, 0.2],
+            console: None,
         }
     }
 
     #[wasm_bindgen]
-    pub fn draw(&mut self, ctx: &CanvasRenderingContext2d, micros: u32) -> Result<(), JsValue> {
-        self.elapsed_micros += micros;
-
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let start = 4 * (y * self.width + x);
-                self.pixels[start] = (100 * self.elapsed_micros / 1_000_000) as u8;
-                self.pixels[start + 1] = x as u8;
-                self.pixels[start + 2] = y as u8;
-                self.pixels[start + 3] = 0xFF;
-            }
+    pub fn update_buttons(&mut self, state: u8) {
+        if let Some(console) = &mut self.console {
+            console.update_controller(ludus::ButtonState {
+                a: state & 1 != 0,
+                b: (state >> 1) & 1 != 0,
+                start: (state >> 2) & 1 != 0,
+                select: (state >> 3) & 1 != 0,
+                up: (state >> 4) & 1 != 0,
+                down: (state >> 5) & 1 != 0,
+                left: (state >> 6) & 1 != 0,
+                right: (state >> 7) & 1 != 0,
+            })
         }
-        let data = ImageData::new_with_u8_clamped_array_and_sh(
-            Clamped(&mut self.pixels),
-            self.width as u32,
-            self.height as u32,
-        )?;
-        ctx.put_image_data(&data, 0.0, 0.0)
+    }
+
+    #[wasm_bindgen]
+    pub fn swap_cart(&mut self, rom: &[u8]) {
+        let cart = ludus::Cart::from_bytes(rom).unwrap();
+        self.console = Some(ludus::Console::new(cart, SAMPLE_RATE));
+    }
+
+    #[wasm_bindgen]
+    pub fn step(&mut self, ctx: &CanvasRenderingContext2d, micros: u32) -> Result<(), JsValue> {
+        if let Some(console) = &mut self.console {
+            console.step_micros(&mut NullAudioDevice, &mut self.pixels, micros);
+        }
+        self.pixels.render_to(ctx)
     }
 }
